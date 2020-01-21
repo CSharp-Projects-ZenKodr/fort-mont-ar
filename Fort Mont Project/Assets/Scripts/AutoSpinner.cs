817b3f822a175c63bdf942e5cbf7240e2bd4d7ff
Daulton Nelson <60152571+DaultonNelson@users.noreply.github.com>
2020-01-21 16:18:32 -0500

    Adding Existing Project

---
+++ Fort Mont Project/Assets/Scripts/AutoSpinner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSpinner : MonoBehaviour {
+
+    #region Public Variables
+    /// <summary>
+    /// Float value for the speed at which the GameObject will rotate.
+    /// </summary>
+    [Tooltip("Float value for the speed at which the GameObject will rotate.")]
+    public float rotationSpeed = 0.0f;
+    #endregion
+	
+	void Update () {
+        SpinItAround();
+	}
+
+    void SpinItAround() {
+        transform.Rotate(Vector3.down, rotationSpeed * Time.deltaTime);
+    }
+}
